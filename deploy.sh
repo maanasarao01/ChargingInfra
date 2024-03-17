e1774ef9027a313d91c5500713a1eae897da2431
maanasarao01 <maanasarao01@gmail.com>
2024-03-17 16:47:10 +0530

    working on shell script

--- deploy.sh
+++ deploy.sh
@@ -15,14 +15,17 @@ docker-compose ps
 payload_file="payload.json"
 
 #read the contents of file
-documents=$(cat "$payload_file")
+documents=$(jq -c '.[]' "$payload_file")
+
+# Display the contents of the file
+echo "$documents"
 
 #sends a post request to post all the documents in payload.json
- for document in $documents
- do
-    # Send a POST request with the current document 
+ echo "$documents" | while IFS= read -r document; do
+    echo "$document"
+    sleep 2
     curl -X POST -H "Content-Type: application/json" -d "$document" http://localhost:3003/charging-stations/connectors
- done
+done
 
 #get Connectors for a given location
 curl "http://localhost:3003/charging-stations/connectors/Girinagar/DC%20Fast%20Charging"
